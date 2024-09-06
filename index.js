5c3c8fcf0c0a0c42ee7a1b26714e02f684fae73b
Jappanjot26 <singhjappanjot1@gmail.com>
2024-09-06 21:07:12 +0530

    Stucturing Class for Transaction

--- index.js
+++ index.js
@@ -14,7 +14,34 @@ dashTab.addEventListener("click", () => {
   transactionMenu.classList.add("hidden");
   dashboard.classList.remove("hidden");
 });
+
 transactionTab.addEventListener("click", () => {
   transactionMenu.classList.remove("hidden");
   dashboard.classList.add("hidden");
 });
+
+class Transaction {
+  #date = new Date();
+  constructor(amount, category, type) {
+    this.date = this.formatDate();
+    this.id = this.#date.getTime();
+    this.amount = amount;
+    this.category = category;
+    this.type = type;
+    console.log(this.date, this.id, this.amount, this.category, this.type);
+  }
+
+  formatDate() {
+    let date = this.#date.getDate();
+    let month = this.#date.getMonth() + 1;
+    const year = this.#date.getFullYear();
+
+    date = date < 10 ? `0${date}` : date;
+    month = month < 10 ? `0${month}` : month;
+
+    return `${date}/${month}/${year}`;
+  }
+
+}
+
+const t1 = new Transaction(140, "food", "credit");
