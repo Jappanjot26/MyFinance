1912b6df1441f541e8c48d31f0f05fb494a428b4
Jappanjot26 <singhjappanjot1@gmail.com>
2024-09-20 20:52:28 +0530

    Implementing Graphs using Plotly.js

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ const transactionTab = document.querySelector("#transaction-tab");
 const menuAmount = document.getElementById("add-amount");
 const menuCategory = document.getElementById("add-category");
 const menuType = document.getElementById("add-type");
+const editAmount = document.getElementById("edit-amount");
+const editCategory = document.getElementById("edit-category");
+const editType = document.getElementById("edit-type");
 
 const addBtn = document.querySelector(".transaction-main .head .filters .add");
 const addMenu = document.querySelector(".transaction-main .add-transaction");
-const saveBtn = document.querySelector(".save-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
+const editMenu = document.querySelector(".transaction-main .edit-transaction");
+const saveBtnAdd = document.querySelector(".add-transaction .btns .save-btn");
+const cancelBtnAdd = document.querySelector(
+  ".add-transaction .btns .cancel-btn"
+);
+const saveBtnEdit = document.querySelector(".edit-transaction .btns .save-btn");
+const cancelBtnEdit = document.querySelector(
+  ".edit-transaction .btns .cancel-btn"
+);
 const balanceAmount = document.querySelector(".balance .amount");
 const creditAmount = document.querySelector(".credit .amount");
 const expenseAmount = document.querySelector(".expense .amount");
 const removeBtn = document.querySelector(".remove");
 const checkbox = document.querySelector("#transaction-checked");
+let saveEdit;
+let cancelEdit;
 
 dashTab.addEventListener("click", () => {
   transactionMenu.classList.add("hidden");
@@ -84,65 +96,71 @@ class Transaction {
     this.calcAndDisplayAmounts();
   }
 
-  editTransaction() {
-    saveBtn.removeEventListener("click", this.saveBtnAdd);
-    saveBtn.addEventListener("click", (e) => this.saveBtnEdit(e));
-    cancelBtn.addEventListener("click", () => {
-      addMenu.classList.add("hidden");
-      menuAmount.value = menuCategory.value = menuType.value = "";
-    });
+  editTransaction(e) {
+    let id = e.target.closest("tr").dataset.id;
+
+    addMenu.classList.add("hidden");
+    editMenu.classList.remove("hidden");
+
+    let target = transactions.find((res) => res.id == id);
+    editAmount.value = target.amount;
+    editCategory.value = target.category;
+    editType.value = target.type;
+
+    saveBtnEdit.removeEventListener("click", saveEdit);
+    cancelBtnEdit.removeEventListener("click", cancelEdit);
+
+    saveEdit = () => {
+      if (
+        editAmount.value !== "" &&
+        editCategory.value !== "" &&
+        editType.value !== ""
+      ) {
+        target.amount = editAmount.value;
+        target.type = editType.value;
+        target.category = editCategory.value;
+      }
+      transactionRow.innerHTML = "";
+
+      transactions.forEach((res) => {
+        res.renderTransaction();
+      });
+      editMenu.classList.add("hidden");
+    };
+
+    cancelEdit = () => {
+      editMenu.classList.add("hidden");
+      editAmount.value = editCategory.value = editType.value = "";
+    };
+
+    saveBtnEdit.addEventListener("click", saveEdit);
+    cancelBtnEdit.addEventListener("click", cancelEdit);
   }
 
   addTransaction() {
+    editMenu.classList.add("hidden");
     addMenu.classList.toggle("hidden");
     menuAmount.value = menuCategory.value = menuType.value = "";
 
-    saveBtn.removeEventListener("click", this.saveBtnEdit);
-    saveBtn.addEventListener("click", this.saveBtnAdd);
-    cancelBtn.addEventListener("click", () => {
+    saveBtnAdd.addEventListener("click", function () {
+      const t = new Transaction(
+        menuAmount.value,
+        menuCategory.value,
+        menuType.value
+      );
+
+      if (t.amount !== "" && t.category !== "" && t.type !== "") {
+        transactions.push(t);
+        t.renderTransaction();
+      }
+      menuAmount.value = menuCategory.value = menuType.value = "";
+      addMenu.classList.add("hidden");
+    });
+
+    cancelBtnAdd.addEventListener("click", () => {
       addMenu.classList.add("hidden");
       menuAmount.value = menuCategory.value = menuType.value = "";
     });
-  }
-
-  saveBtnEdit(e) {
-    const id = e.target.closest("tr").dataset.id;
-    addMenu.classList.remove("hidden");
-
-    const target = transactions.find((res) => res.id == id);
-    menuAmount.value = target.amount;
-    menuCategory.value = target.category;
-    menuType.value = target.type;
-    addMenu.classList.add("hidden");
-    if (
-      menuAmount.value !== "" &&
-      menuCategory.value !== "" &&
-      menuType.value !== ""
-    ) {
-      target.amount = menuAmount.value;
-      target.type = menuType.value;
-      target.category = menuCategory.value;
-    }
-    transactionRow.innerHTML = "";
-
-    transactions.forEach((res) => {
-      res.renderTransaction();
-    });
-  }
-
-  saveBtnAdd() {
-    const t = new Transaction(
-      menuAmount.value,
-      menuCategory.value,
-      menuType.value
-    );
-    console.log(t);
-
-    if (t.amount !== "" && t.category !== "" && t.type !== "") {
-      transactions.push(t);
-      t.renderTransaction();
-    }
-    menuAmount.value = menuCategory.value = menuType.value = "";
   }
 
   calcAndDisplayAmounts() {
@@ -162,6 +180,7 @@ class Transaction {
 
   removeTransaction() {
     addMenu.classList.add("hidden");
+    editMenu.classList.add("hidden");
     let c = 0;
     transactions = transactions.filter((res) => res.check === false);
     transactionRow.innerHTML = "";
@@ -178,18 +197,108 @@ class Transaction {
   }
 }
 
-const t1 = new Transaction(170, "food", "credit");
+const t1 = new Transaction(170, "savings", "credit");
 transactions.push(t1);
 for (let i = 0; i < 100000000; i++);
-const t2 = new Transaction(145, "clothes", "expense");
+const t2 = new Transaction(145, "grocery", "expense");
 transactions.push(t2);
 for (let i = 0; i < 100000000; i++);
-const t3 = new Transaction(256, "clothes", "credit");
+const t3 = new Transaction(256, "grooming", "expense");
+t3.date = "19/09/2024";
 transactions.push(t3);
 for (let i = 0; i < 100000000; i++);
-const t4 = new Transaction(15, "clothes", "expense");
+const t4 = new Transaction(15, "rent", "expense");
+t4.date = "19/09/2024";
 transactions.push(t4);
 
 transactions.forEach((res) => {
   res.renderTransaction();
 });
+
+const getWeeklyExpenses = (transactions) => {
+  const today = new Date();
+  const weekStart = today.getDate() - today.getDay(); 
+  const weeklyExpenses = Array(7).fill(0);
+
+  
+  const startOfWeek = new Date(
+    today.setFullYear(today.getFullYear(), today.getMonth(), weekStart)
+  );
+  const endOfWeek = new Date(
+    today.setFullYear(today.getFullYear(), today.getMonth(), weekStart + 7)
+  );
+
+  transactions.forEach((trans) => {
+    if (trans.type === "expense") {
+      const [day, month, year] = trans.date.split("/").map(Number);
+      const transDate = new Date(year, month - 1, day); 
+
+      
+      if (transDate >= startOfWeek && transDate < endOfWeek) {
+        const dayIndex = transDate.getDay();
+        weeklyExpenses[dayIndex] += Number(trans.amount);
+      }
+    }
+  });
+
+  return weeklyExpenses;
+};
+
+const getCategoryExpenses = (transactions) => {
+  const categories = ["grocery", "travel", "rent", "grooming", "others"];
+  const categoryExpenses = Array(categories.length).fill(0);
+
+  const today = new Date();
+  const weekStart = new Date(today.setDate(today.getDate() - today.getDay()));
+
+  transactions.forEach((trans) => {
+    const [day, month, year] = trans.date.split("/").map(Number);
+    const transDate = new Date(year, month - 1, day);
+
+    if (transDate >= weekStart) {
+      const categoryIndex = categories.indexOf(trans.category);
+      if (categoryIndex !== -1) {
+        categoryExpenses[categoryIndex] += Number(trans.amount);
+      }
+    }
+  });
+
+  return categoryExpenses;
+};
+
+const barData = [
+  {
+    x: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    y: getWeeklyExpenses(transactions),
+    type: "bar",
+    marker: {
+      color: "#26B2EC",
+    },
+  },
+];
+
+const pieData = [
+  {
+    values: getCategoryExpenses(transactions),
+    labels: ["Grocery", "Travel", "Rent", "Gromming", "Others"],
+    type: "pie",
+    textinfo: "percent",
+    insidetextorientation: "radial",
+    marker: {
+      colors: ["#FF6384", "#36A2EB", "#FFCE56"],
+    },
+  },
+];
+
+const layout = {
+  margin: { t: 50, l: 35, r: 0, b: 20 }, 
+  responsive: true, 
+};
+
+Plotly.newPlot("barGraph", barData, layout);
+Plotly.newPlot("pieChart", pieData, layout);
+
+window.onresize = function () {
+  Plotly.Plots.resize("barGraph");
+  Plotly.Plots.resize("pieChart");
+};
